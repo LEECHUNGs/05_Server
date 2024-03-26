2417d1d59cc4359ff8dbdc92649b53766e250fe9
LEECHUNGs <ragnacrimson@naver.com>
2024-03-26 17:45:12 +0900

    Server TodoList

---
+++ TodoList/src/main/java/edu/kh/todoList/member/model/dto/Member.java
@@ -0,0 +1,23 @@
+package edu.kh.todoList.member.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Member {
+	
+	// lombok 라이브러리 : getter/setter, 생성자, toString 자동완성 라이브러리
+	private int memberNo;
+	private String memberId;
+	private String memberPw;
+	private String memberNickname;
+	private String enrollDate;
+	private String memberDeleteFlage;
+}
